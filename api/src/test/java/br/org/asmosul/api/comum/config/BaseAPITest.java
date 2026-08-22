dde95bf675fe6940e41ef72b07eb702795f2c066
Victor Alexandre B M <valex.milhomem@gmail.com>
2026-08-22 08:08:05 -0300

    chore: adiciona dependência de testcontainers e um teste base. Remove dependência de banco h2

---
+++ api/src/test/java/br/org/asmosul/api/comum/config/BaseAPITest.java
@@ -0,0 +1,23 @@
+package br.org.asmosul.api.comum.config;
+
+import jakarta.transaction.Transactional;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Testcontainers
+@Transactional
+public abstract class BaseAPITest {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer mysql = new MySQLContainer("mysql:8.0")
+            .withDatabaseName("asmosul_db_test")
+            .withUsername("test")
+            .withPassword("test");
+}
