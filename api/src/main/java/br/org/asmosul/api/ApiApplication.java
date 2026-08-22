b2e3454dce9d3fa1014e214cde08a3de422b3c64
Victor Alexandre B M <valex.milhomem@gmail.com>
2026-08-21 22:06:44 -0300

    chore: inicia projeto spring backend

---
+++ api/src/main/java/br/org/asmosul/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package br.org.asmosul.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/br/org/asmosul/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.asmosul.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
