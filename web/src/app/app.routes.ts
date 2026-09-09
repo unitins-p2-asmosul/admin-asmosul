d155597332934288f34ef6d1cdadd2b32b905a70
diegogalvao-dev <diegogalvaoamorim@gmail.com>
2026-09-09 10:20:05 -0300

    layout frontend

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 	{
+		path: '',
+		loadComponent: () => import('./home.component').then((modulo) => modulo.HomeComponent),
+	},
+	{
 		path: 'pessoas',
 		loadChildren: () =>
 			import('@features/pessoas/pessoas.routes').then((modulo) => modulo.PESSOAS_ROUTES),
