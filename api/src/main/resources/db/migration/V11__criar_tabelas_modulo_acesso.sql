6183654e711868f0c9b32e1c3364b1ca5442cb10
Victor Alexandre Borges Milhomem <valex.milhomem@gmail.com>
2026-09-22 20:47:26 -0300

    renomeia tabelas em migração

--- api/src/main/resources/db/migration/V11__criar_tabelas_modulo_acesso.sql
+++ api/src/main/resources/db/migration/V11__criar_tabelas_modulo_acesso.sql
@@ -6,13 +6,13 @@ CREATE TABLE conta (
     senha_hash VARCHAR(255) NOT NULL,
     data_criacao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     data_inativo DATETIME NULL,
-    CONSTRAINT fk_contas_pessoas FOREIGN KEY (id_pessoa) REFERENCES pessoas(id)
+    CONSTRAINT fk_conta_pessoa FOREIGN KEY (id_pessoa) REFERENCES pessoa(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Criar tabela de perfis de conta
-CREATE TABLE contas_perfis (
+CREATE TABLE conta_perfil (
     id_conta BIGINT NOT NULL,
     perfil VARCHAR(50) NOT NULL,
     PRIMARY KEY (id_conta, perfil),
-    CONSTRAINT fk_contas_perfis_contas FOREIGN KEY (id_conta) REFERENCES conta(id) ON DELETE CASCADE
+    CONSTRAINT fk_conta_perfil_conta FOREIGN KEY (id_conta) REFERENCES conta(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
